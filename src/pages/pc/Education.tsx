6608e9666cb28aed53bde05a6aea57fcec2dd2ec
Leo <heesu12200@gmail.com>
2022-12-19 16:05:22 -0600

    add pages

---
+++ src/pages/pc/Education.tsx
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+
+
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react'
+import styled from "@emotion/styled";
+const StyledButton = styled.button`
+  padding: 6px 12px;
+  border-radius: 8px;
+  font-size: 1rem;
+  line-height: 1.5;
+  border: 1px solid lightgray;
+  color: gray;
+  background: white;
+`;
+export default () => {
+
+
+    return(
+        <StyledButton>ddd</StyledButton>
+    )
+}
