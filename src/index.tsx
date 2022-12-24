a9a9c979d4c9293d6ceee778118fbd15eb2dfe55
Leo <heesu12200@gmail.com>
2022-12-24 13:16:37 -0600

    apply message (en,kr)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App"
 import "./tailwind.css"
+import "./locale/i18n";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
